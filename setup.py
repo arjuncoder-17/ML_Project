1b1b4d11a5a157da83612331198f99c69aba3cd7
arjuncoder-17 <arjun.gupta.nit17@gmail.com>
2024-09-19 13:34:21 +0530

    Logging and exception

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import find_packages, setup
 from typing import List
-
+HYPHEN_E_DOT='-e .' 
 def get_requirements(file_path:str)->List[str]:
   '''
   this function will return the list of requirements
@@ -12,7 +12,10 @@ def get_requirements(file_path:str)->List[str]:
     requirements=file_obj.readlines()
     requirements=[req.replace("\n","") for req in requirements]
 
-
+    if HYPHEN_E_DOT in requirements:
+      requirements.remove(HYPHEN_E_DOT)
+      
+  return requirements
 
 setup(
 name="ML_PROJECT",
